4a43debbed83b21e4da4179c8844444f551f0ef7
Nikita <Roux933@mail.ru>
2023-03-07 18:04:01 +0300

    Zadacha4

--- Zadacha2/Program.cs
+++ Zadacha2/Program.cs
@@ -3,11 +3,14 @@
 Console.WriteLine("Введите 2 целых числа ");
 int num1 = Convert.ToInt32( Console.ReadLine() );
 int num2 = Convert.ToInt32( Console.ReadLine() );
+
 if (num1 > num2)
 {
     Console.WriteLine("Максимальное число = " + num1);
+    Console.WriteLine("Минимальное число = " + num2);
 }
 else
 {
     Console.WriteLine("Максимальное число = " + num2);
+    Console.WriteLine("Минимальное число = " + num1);
 }
